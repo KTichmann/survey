59ce9ff2df8e2e0c9efb34f7e012714724f9b23f
KTichmann <ktichmann@gmail.com>
2018-04-02 13:01:15 +0200

    changed routes

--- routing.js
+++ routing.js
@@ -6,50 +6,59 @@ const router = () => {
       item.classList.remove("active")
     })
   }
-  Router.route("/", {
+  const base = window.location.origin + "/survey/"
+  const removeDisabledFromLinks = () => {
+    document.querySelectorAll(".final").forEach(node => {
+      node.classList.remove("disabled")
+    })
+  }
+  Router.route(base, {
       name: "home",
       action() {
-        this.render(`<h1>Welcome to our Survey!</h1><br><p>This will only take 5 years to fill out, please begin by filling in your name etc.</p>`, {
+        this.render(`<h1>Welcome to our Survey!</h1><br><p>This will only take 5 years to fill out, please begin by filling in your name.</p>`, {
           target: "content"
         })
       }
   });
-  Router.route("/question1", {
+  Router.route(base + "/question1", {
       name: "question1",
       action() {
-        removeActiveFromLinks()
+        removeActiveFromLinks();
+        removeDisabledFromLinks();
         document.getElementById("tree-main").style.display = "block"
         document.getElementById("form-inputs").style.display = "none"
         document.getElementById("input").textContent = "Next Question"
-        this.render(`<h1>Question 1:</h1>`, {
+        this.render(`<h1>Question 1:</h1><br><p>If you were looking for information about assessment, where do you think you would find it?</p>`, {
           target: "content"
         })
       }
   });
-  Router.route("/question2", {
+  Router.route(base + "/question2", {
       name: "question2",
       action() {
-        removeActiveFromLinks()
-        this.render(`<h1>Question 2:</h1>`, {
+        removeActiveFromLinks();
+        removeDisabledFromLinks();
+        this.render(`<h1>Question 2:</h1><br><p>If you were looking for information about mentoring, where do you think you would find it?</p>`, {
           target: "content"
         })
       }
   });
-  Router.route("/question3", {
+  Router.route(base + "/question3", {
       name: "question3",
       action() {
-        removeActiveFromLinks()
-        this.render(`<h1>Question 3:</h1>`, {
+        removeActiveFromLinks();
+        removeDisabledFromLinks();
+        this.render(`<h1>Question 3:</h1><br><p>If you were looking for a case study from Ghana, where do you think you would find it?</p>`, {
           target: "content"
         })
       }
   });
-  Router.route("/thankyou", {
+  Router.route(base + "/thankyou", {
       name: "thankyou",
       action() {
-        document.getElementById("tree-main").style.display = "none"
-        document.getElementById("input").style.display = "none"
-        this.render(`<h1>Thanks!</h1>`, {
+        document.getElementById("tree-main").style.display = "none";
+        document.getElementById("input").style.display = "none";
+        this.render(`<h1>Thank You!</h1><br><p>Thanks for taking the time to complete our survey, your answers have been saved. You may now exit this tab. <br> For more information, please visit <a href="#">this site</a>.</p>`, {
           target: "content"
         })
       }
